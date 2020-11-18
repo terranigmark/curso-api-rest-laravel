57815d8fc4b337c75a03208fd22118f0ccd04ff2
italomoralesf <profesoritalomoralesf@gmail.com>
2020-11-18 15:06:17 -0500

    Tailswind

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,14 +5,23 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Laravel</title>
+
+        <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
     </head>
-    <body>
-        @foreach($posts as $post)
-        <div>
-            <h2>{{ $post->title }}</h2>
+    <body class="bg-gray-100 text-gray-700">
+        <div class="container mx-auto px-4">
+            <div class="grid grid-cols-3 my-10">
+                @foreach($posts as $post)
+                <div class="bg-white hover:bg-blue-100 border border-gray-200 p-5">
+                    <h2 class="font-bold text-lg mb-4">{{ $post->title }}</h2>
+                    <p class="text-xs">{{ $post->excerpt }}</p>
+                    <p class="text-xs text-right">{{ $post->published_at }}</p>
+                </div>
+                @endforeach
+            </div>
+            <div class="mb-10">
+                {{ $posts->links() }}
+            </div>
         </div>
-        @endforeach
-        
-        {{ $posts->links() }}
     </body>
 </html>

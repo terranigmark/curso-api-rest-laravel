5e97a386425d8a5e6703ddaa9ae1c61eff38aae5
italomoralesf <profesoritalomoralesf@gmail.com>
2020-11-18 16:35:35 -0500

    Versión 2

---
+++ app/Http/Resources/V2/PostCollection.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Resources\V2;
+
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class PostCollection extends ResourceCollection
+{
+
+    public $collects = PostResource::class;
+
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'data' => $this->collection,
+            'meta' => [
+                'organization' => 'Platzi',
+                'authors' => [
+                    'Italomoralesf',
+                    'Platzi'
+                ]
+            ],
+            'type' => 'articles'
+        ];
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,5 +3,13 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('v1/posts', App\Http\Controllers\Api\V1\PostController::class)
+use App\Http\Controllers\Api\V1\PostController as PostV1;
+use App\Http\Controllers\Api\V2\PostController as PostV2;
+
+// V1
+Route::apiResource('v1/posts', PostV1::class)
     ->only(['index', 'show', 'destroy']);
+
+// V2
+Route::apiResource('v2/posts', PostV2::class)
+->only(['index', 'show']);
